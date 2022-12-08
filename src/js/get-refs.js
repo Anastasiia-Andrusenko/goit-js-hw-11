56d6993d4c09ff060b07200d5c6e5cc814a6fc33
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2022-12-08 17:43:54 +0200

    Create files

---
+++ src/js/get-refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    form: document.querySelector('#search-form"'),
+    input: document.querySelector(''),
+    gallery: document.querySelector('.gallery'),
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
+// --------- ІМПОРТ БІБЛІОТЕК
 import axios from "axios";
 import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+// --------- ІМПОРТ ФЕТЧА
+import { fetchImages } from "./fetchImgs";
+
+// --------- ІМПОРТ РЕФІВ
+import getRefs from "./get-refs";
+
+
+const string = 'cat';
+
+fetchImages(string);
